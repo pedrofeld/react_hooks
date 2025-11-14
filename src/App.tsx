932ec5d601dd3da57dd17a9b59b9a690f6aa1f71
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-13 22:08:44 -0300

    feat: criando um cronômetro utilizando useState e sets

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
 import { useState, useEffect } from 'react';
 
-function App() {
-  // useState: cria estado 'count' com valor inicial 0
-  const [count, setCount] = useState(0);
+function Timer() {
+  const [seconds, setSeconds] = useState(0)
 
-  // useEffect: sincroniza o título da página com o estado do contador
-  // executa quando 'count' muda
   useEffect(() => {
-    document.title = `Contador: ${count}`;
-  }, [count]); // Array de dependências
+    const interval = setInterval(() => {
+      setSeconds(prevState => prevState + 1)
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+      console.log("Intervalo limpo")
+    }
+  }, [])
+
+  return <p>Segundos: {seconds}</p>
+}
+
+function App() {
+  /*
+    // useState: cria estado 'count' com valor inicial 0
+    const [count, setCount] = useState(0);
+
+    // useEffect: sincroniza o título da página com o estado do contador
+    // executa quando 'count' muda
+    useEffect(() => {
+      document.title = `Contador: ${count}`;
+    }, [count]); // Array de dependências
+  */
+
+  const [showTimer, setShowTimer] = useState(false);
 
   return (
-    <div>
-      <p>Você clicou {count} vezes</p>
-      <button onClick={() => setCount(count + 1)}>
-        Clique aqui
+    <>
+      {/*
+        <p>Você clicou {count} vezes</p>
+        <button onClick={() => setCount(count + 1)}>
+          Clique aqui
+        </button>
+      */}
+      <button onClick={() => setShowTimer(!showTimer)}>
+        {showTimer ? "Parar timer" : "Iniciar timer"}
       </button>
-    </div>
+      {showTimer && <Timer/>}
+    </>
   );
 }
 
