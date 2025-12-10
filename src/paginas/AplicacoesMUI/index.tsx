25f02c97943e6dfd1e6eec2c968e4749e66c9006
Pedro Feld <pedro.gael.feld@gmail.com>
2025-12-10 20:54:21 -0300

    style: testando alguns componentes do MUI

--- src/paginas/AplicacoesMUI/index.tsx
+++ src/paginas/AplicacoesMUI/index.tsx
@@ -1,4 +1,5 @@
-import { TextField, Typography } from '@mui/material';
+import { Button, TextField, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
 // import { CssBaseline } from '@mui/material';
 
 import Navbar from '../../components/Navbar';
@@ -8,8 +9,31 @@ export default function MUI() {
     <>
         <Navbar/>
         {/*<CssBaseline/>*/}
+
         <Typography>Teste de texto</Typography>
-        <TextField label="Nome"/>
+
+        <TextField label="Nome" slotProps={{
+          input: {
+            sx: {
+              backgroundColor: "gray.200",
+              borderRadius: 6
+            },
+          },
+        }}/>
+
+        <p>Grid:</p>
+        <Grid container spacing={2}>
+          <Grid size={{xs: 6, md: 8}}>
+            xs: 6, md: 8
+          </Grid>
+          <Grid size={{xs: 6, md: 4}}>
+            xs: 6, md: 4
+          </Grid>
+        </Grid>
+        
+        <Button variant="contained" color="primary" sx={{ borderRadius: 8, backgroundColor: "secondary.light"}}>
+          Botão
+        </Button>
     </>
   );
 }
